887b6f84b41f09ed229e5278724c57916ac4480d
Admish-prasoon <98838056+Admish-prasoon@users.noreply.github.com>
2022-02-05 13:09:42 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+#!/usr/bin/python
+# coding=utf-8
+# coded by : ADHEESH-MISHRA
+#Blog : https://adheesh-mishra-code.blogspot.com
+
+print("\033[1;93m         First Tool login or signup by Adheesh-Mishra")
+print("\033[1;92m hi!")
+
+username = input("\033[1;95m[+]\033[1;95m what is your name:\033[1;92m ")
+
+email = input("\033[1;95m[+]\033[1;95m what is your email:\033[1;92m ")
+
+pas = input("\033[1;95m[+]\033[1;95m what is your password:\033[1;92m ")
+
+print("\033[1;92m wellcome " + username)
+
+print("Details")
+
+username = print("\033[1;95m[+]\033[1;92m Username : " + username)
+
+username = print("\033[1;95m[+]\033[1;92m email : " + email)
+
+pas = print("\033[1;95m[+]\033[1;92m password : " + pas)
+
+print("\033[1;97m")
